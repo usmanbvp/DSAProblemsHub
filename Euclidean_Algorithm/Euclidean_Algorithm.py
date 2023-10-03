4697bcda63dcebc35af5e13b6b06db0c882c79f0
Chandu2000 <62535597+Chandu2000@users.noreply.github.com>
2023-10-03 18:11:54 +0530

    Euclidean_Algorithm.py
    Euclidean Algorithm

---
+++ Euclidean_Algorithm/Euclidean_Algorithm.py
@@ -0,0 +1,15 @@
+def GCD(a,b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+c = int(input())
+d = int(input())
+print(GCD(c,d))
+#input
+#24
+#30
+#output
+#6
