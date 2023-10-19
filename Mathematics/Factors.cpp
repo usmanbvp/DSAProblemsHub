e9e80303a17263e6ac1f987c8ed7c371eb7ade30
AK3847 <abdulkadir.co21d2@scet.ac.in>
2023-10-19 19:03:34 +0530

    Modified the Code

--- Mathematics/Factors.cpp
+++ Mathematics/Factors.cpp
@@ -7,24 +7,27 @@
 using namespace std;
 using ll=long long; //using long long data structure for large numbers 
 
-void allfacts(ll n, vector<ll> &v){
+void allfacts(ll n, vector<ll> &v) //user-defined function to find factors and storing them in a Vector
+{
     for(ll i=1; i<=sqrt(n); ++i){
         if(n%i==0){
-        if(n/i==i)
-            v.push_back(i);
-        else{
-            v.push_back(i);
-            v.push_back(n/i);
+            if(n/i==i)
+                v.push_back(i);
+            else{
+                v.push_back(i);
+                v.push_back(n/i);
+            }
         }
-}}}
+    }
+}
 int main()
 {
     ll num;
     cout<<"Give your number: ";
     cin>>num;
     vector<ll> factors;
-    allfacts(num,factors);
-    sort(factors.begin(),factors.end());
+    allfacts(num,factors); //calling the user-defined function
+    sort(factors.begin(),factors.end()); //sorting the vector so as to get factors in ascending order
     ll sz=factors.size();
     cout<<"The factors of given number "<<num<<" are:\n";
     for(ll it=0;it<sz;it++)
